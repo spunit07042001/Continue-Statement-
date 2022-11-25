9f928dc1a972d18bb2efd8ba6ed3deea2171fd83
Punit Soni <85535330+spunit07042001@users.noreply.github.com>
2022-11-25 22:26:34 +0530

    Add files via upload

---
+++ continueStatement.java
@@ -0,0 +1,16 @@
+public class continueStatement {
+    public static void main(String args[]) {
+        // Scanner sc = new Scanner (System.in);
+
+        for (int i=1; i<=5; i++)
+        {
+            if (i==3)// skit 4 from the loop
+            {
+                continue;
+            }
+            System.out.println(i);
+        }
+        
+    }
+    
+}
